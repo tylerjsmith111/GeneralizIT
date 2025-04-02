4cc31fe222d14df82758ff9c0e7c75fd7d5fd58f
Tyler Smith <tyler.smith2@northwestern.edu>
2025-04-02 17:44:45 -0500

    Correcting test missing fixture

--- tests/fixtures/design1_missing_fixtures.py
+++ tests/fixtures/design1_missing_fixtures.py
@@ -11,7 +11,7 @@ def test_design1_missing():
     # Brennan (2001) Synthetic Data # 1
     
     # First we parse the input string to get the research design
-    design_str = "person x item"
+    design_str = "p x i"
     design_num, facets = match_research_design(design_str)
     
     # Validate the research design
@@ -22,80 +22,67 @@ def test_design1_missing():
 
     variance_tuple_dictionary = parse_facets(design_num=design_num, design_facets=facets)
 
-    data = [
-        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0],
-        [1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    data_raw = [
+        [1, 1, 1, 1, 1, 0, 1],
+        [2, 1, 0, 1, 1, 1, 1],
+        [3, None, 1, 1, 1, 0, 0],
+        [4, 0, None, None, 1, 0, 1],
+        [5, 1, None, 0, 1, 0, None],
+        [6, 1, 1, 1, 1, 0, 1],
+        [7, 0, None, 1, None, 0, 0],
+        [8, 0, None, 1, 1, 1, 1],
+        [9, 1, 1, 0, 1, None, 0],
+        [10, None, 0, 0, 0, None, 0],
+        [11, 1, 1, 1, 1, 1, 1],
+        [12, None, 0, None, 0, 0, 1]
     ]
 
+    # Create DataFrame with appropriate column names
+    data = pd.DataFrame(data_raw, columns=['p', 'i_1', 'i_2', 'i_3', 'i_4', 'i_5', 'i_6'])
+
     formatted_data = {
-        'person': [],
-        'item': [],
+        'p': [],
+        'i': [],
         'Response': []
     }
 
-    for person in range(1, 11):
-        for item in range(1, 13):
-            formatted_data['person'].append(person)
-            formatted_data['item'].append(item)
-            formatted_data['Response'].append(data[person-1][item-1])
+    for person in range(1, 13):
+        for item in range(1, 7):
+            response = data.loc[data['p'] == person, f'i_{item}'].values[0]
+            # Only add non-null values
+            if pd.notna(response):
+                formatted_data['p'].append(person)
+                formatted_data['i'].append(item)
+                formatted_data['Response'].append(response)
 
     synthetic_data = pd.DataFrame(formatted_data)
 
     t_values_dict = {
-        'person': 44.75,
-        'item': 47.10,
-        'person x item': 67.00
+        'p': 27.8,
+        'i': 24.7432,
+        'p x i': 37.0,
+        'mean': 23.2034
     }
 
 
     variances_dict = {
-        'person': 0.0574,
-        'item': 0.0754,
-        'person x item': 0.1269
+        'p': 0.0473,
+        'i': 0.0117,
+        'p x i': 0.1840
     }
 
     rho_dict = {
-        'person': 0.844
+        'p': 0.5507
     }
 
     phi_dict = {
-        'person': 0.773
-    }
-    
-    # ---- Unused ----
-    levels_dict = {
-        'person': 10,
-        'item': 12
+        'p': 0.5354
     }
 
-    deg_freedom_dict = {
-        'person': 9,
-        'item': 11,
-        'person x item': 99
-    }
-    
-    ss_values_dict = {
-        'person': 7.3417,
-        'item': 9.6917,
-        'person x item': 12.5583
-    }
 
-    ms_values_dict = {
-        'person': 0.8157,
-        'item': 0.8811,
-        'person x item': .1269
-    }
 
     return DesignTestBase(
-        design=Design(data=synthetic_data, variance_tuple_dictionary=variance_tuple_dictionary, response_col='Response'),
+        design=Design(data=synthetic_data, variance_tuple_dictionary=variance_tuple_dictionary, response_col='Response', missing_data=True),
         levels_df=None,
         t_values_dict=t_values_dict,
         variances_dict=variances_dict,
