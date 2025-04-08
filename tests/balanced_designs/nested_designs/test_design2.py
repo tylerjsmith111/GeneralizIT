43c0ddb7641beff66e280288bf7747012e866cb6
Tyler Smith <tyler.smith2@northwestern.edu>
2025-04-07 19:08:43 -0500

    Add: Implement test_d_coefficients to verify D-study calculations with adjusted utility function

--- tests/balanced_designs/nested_designs/test_design2.py
+++ tests/balanced_designs/nested_designs/test_design2.py
@@ -33,6 +33,12 @@ def test_g_coefficients(test_design2):
     
 @pytest.mark.balanced
 @pytest.mark.nested
+def test_d_coefficients(test_design2):
+    """Tests the calculation of G coefficients."""
+    test_design2.set_tolerance(0.01).test__calculate_d_study(d_study_design={'person': [10], 'item': [8]}, utils=True)
+    
+@pytest.mark.balanced
+@pytest.mark.nested
 def test_confidence_intervals(test_design2):
     """Tests the calculation of confidence intervals."""
     test_design2.set_tolerance(0.01).test__calculate_confidence_intervals()
